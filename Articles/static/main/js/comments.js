fffb1d5eae1d6d4ea69da480089666e757e31f30
Kamran <kamrankek48@gmail.com>
2024-02-16 20:05:59 +0400

    JQuery comments replies + updated menu with icons

--- Articles/static/main/js/comments.js
+++ Articles/static/main/js/comments.js
@@ -1,39 +1,10 @@
-const openReplyButton = document.querySelector('#open_reply_button')
-const closeReplyButton = document.querySelector('#close_reply_button')
+$(document).ready(function(){
+   $(".comment-reply-btn").click(function(event){
+         event.preventDefault();
+         $(this).parent().next(".comment-reply").fadeToggle();
+   })
 
-const replyDiv = document.querySelector('#reply_div')
-const replyForm = document.querySelector('#reply_form')
-
-const commentsCount = document.getElementsByClassName('reply_div')
-
-let elem = document.body
-
-openReplyButton.onclick = function(e) {
-  e.preventDefault()
-
-  openReplyButton.classList.add('hidden')
-  closeReplyButton.classList.remove('hidden')
-
-  replyDiv.classList.remove('hidden')
-
-  console.log('replies are opened!')
-  console.log(commentsCount)
-
-  return false
-}
-
-closeReplyButton.onclick = function(e) {
-  e.preventDefault()
-
-  openReplyButton.classList.remove('hidden')
-  closeReplyButton.classList.add('hidden')
-
-  replyDiv.classList.add('hidden')
-
-  console.log('replies are closed!')
-
-  return false
-}
+})
 
 
 
--- users/models.py
+++ users/models.py
@@ -8,15 +8,16 @@ from users import my_models
 
 class User(AbstractUser):
     followers = models.ManyToManyField('self', related_name='followed_by', symmetrical=False, blank=True)
-    avatar = my_models.AvatarImageField(upload_to='users/avatars', validators=[FileExtensionValidator(
-        allowed_extensions=['jpg', 'jpeg', 'png', 'svg']
-    )], default='users/avatars_default/nursultan.jpeg')
+    avatar = my_models.AvatarImageField(
+        upload_to=f'users/avatars/', validators=[FileExtensionValidator(
+            allowed_extensions=['jpg', 'jpeg', 'png', 'svg']
+        )], default='users/avatars_default/nursultan.jpeg')
     date_birth = models.DateField(null=True)
     is_subscribed_on_quotes = models.BooleanField(default=False, blank=True)
     is_subscribed_on_weather = models.BooleanField(default=False, blank=True)
 
+    def get_username(self):
+        return self.username
+
     def get_rooms(self):
         return self.rooms.all()
-
-
-
