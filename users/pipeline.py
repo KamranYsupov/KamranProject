4beff296f9db96b696b641b3cd0f10588f8a7109
Kamran <kamrankek48@gmail.com>
2024-03-02 09:28:42 +0400

    Fix oauth bugs

--- users/pipeline.py
+++ users/pipeline.py
@@ -4,7 +4,11 @@ from django.contrib.auth.models import Group
 
 def auth_user_form_social(backend, user, *args, **kwargs):
     response = None
-    oauth_group = Group.objects.get(name='oauth')
+    try:
+        oauth_group = Group.objects.get(name='oauth')
+    except TypeError:
+        Group.objects.create(name='oauth')
+        oauth_group = Group.objects.get(name='oauth')
     github_avatar_url = 'https://avatars.githubusercontent.com/'
     if str(backend).__contains__('GithubOAuth2'):
         response = requests.get(github_avatar_url + str(user.username))
